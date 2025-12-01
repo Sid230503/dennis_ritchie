a81c86d215f524748d2dde2936ec9d70ebf096f6
Siddharth23-eic <siddharthsinh.rana@einfochips.com>
2025-12-01 12:02:47 +0530

    DR 7.9

--- src/168863_Siddharthsinh_Rana_Chapter_7/source/168863_Siddharthsinh_Rana_DR_Ch7_E9.c
+++ src/168863_Siddharthsinh_Rana_Chapter_7/source/168863_Siddharthsinh_Rana_DR_Ch7_E9.c
@@ -3,17 +3,49 @@
  * Created Date: 16/11/2025
  * Chapter: 5
  * Exercise: 7.9
- * Description:
+ * Description: Functions like isupper can be implemented to save space or to save time. Explore both possibilities.
  */
 
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
+#include <ctype.h>
 #include "chapter7func.h"
 
-int32_t exercise7_9() {
-    // Your code goes here
+int32_t my_isupper(int32_t c){
+    return (c >= 'A' && c <= 'Z');
+}
 
+int32_t exercise7_9() {
+    
+    int32_t ch;
+    
+    int32_t itr;
+    printf("Number of interations to test performance: ");
+    scanf("%d", &itr);
+
+    int32_t start, end;
+    
+    /* Testing the custom my_isupper() */
+    start = clock();
+    for(int32_t i = 0; i < itr; i++){
+        for(ch = 0; ch <= 127; ch++){
+            my_isupper(ch);
+        }
+    }
+    end = clock();
+    printf("Custom isupper time: %lf seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+
+    /* Testig the inbuilt isupper() */
+    start = clock();
+    for(int32_t i = 0; i < itr; i++){
+        for(ch = 0; ch <= 127; ch++){
+            isupper(ch);
+        }
+    }
+    end = clock();
+    printf("Library isupper time: %lf seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
     return 0;
 }
 
